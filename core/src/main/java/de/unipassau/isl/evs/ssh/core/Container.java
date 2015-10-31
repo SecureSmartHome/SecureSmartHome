3c6e9d31a72720b18c6785bcd42bc568f3c99997
Niko Fink <nfink95@gmail.com>
2015-10-31 23:03:09 +0100

    initial project layout

---
+++ core/src/main/java/de/unipassau/isl/evs/ssh/core/Container.java
@@ -0,0 +1,12 @@
+package de.unipassau.isl.evs.ssh.core;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Container {
+    public static final Set<String> components = new HashSet<>();
+
+    static {
+        components.add("core");
+    }
+}
---
+++ drivers/src/main/java/de/unipassau/isl/evs/ssh/drivers/Driver.java
@@ -0,0 +1,9 @@
+package de.unipassau.isl.evs.ssh.drivers;
+
+import de.unipassau.isl.evs.ssh.core.Container;
+
+public class Driver {
+    static {
+        Container.components.add("drivers");
+    }
+}
