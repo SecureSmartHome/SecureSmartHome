7f34277b50280b9559a95f9181c9a4a0ebdb6877
Niko Fink <nfink95@gmail.com>
2015-11-14 00:48:08 +0100

    Fixed instrumentation tests

---
+++ app/src/androidTest/java/de/unipassau/isl/evs/ssh/app/MainActivityTest.java
@@ -0,0 +1,24 @@
+package de.unipassau.isl.evs.ssh.app;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import de.unipassau.isl.evs.ssh.core.container.Container;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    public void testViews() {
+        assertEquals(
+                "[core]",
+                ((TextView) getActivity().findViewById(R.id.textViewComponents)).getText());
+    }
+
+    public void testDependencies() {
+        //Driver.load();
+        assertTrue("core not available", Container.components.contains("core"));
+        assertFalse("drivers available", Container.components.contains("drivers"));
+    }
+}
