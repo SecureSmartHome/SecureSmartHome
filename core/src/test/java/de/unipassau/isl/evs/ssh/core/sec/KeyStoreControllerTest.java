16c6a9a37c73d3bb6bdef347523bbf6d237689dc
Leon Sell <sellleon@fim.uni-passau.de>
2015-11-16 17:03:46 +0100

    added junit test dependencies that don't need an android device

---
+++ core/src/test/java/de/unipassau/isl/evs/ssh/core/sec/KeyStoreControllerTest.java
@@ -0,0 +1,16 @@
+package de.unipassau.isl.evs.ssh.core.sec;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by leon on 16.11.15.
+ */
+public class KeyStoreControllerTest {
+
+    @Test
+    public void testCalculatePublicKeyFingerprint() throws Exception {
+
+    }
+}
