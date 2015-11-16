f77e0c1846a1affdf9e60f71628ef3ef311d2963
Leon Sell <sellleon@fim.uni-passau.de>
2015-11-16 17:37:06 +0100

    added fingerprint method

--- core/src/main/java/de/unipassau/isl/evs/ssh/core/sec/KeyStoreController.java
+++ core/src/main/java/de/unipassau/isl/evs/ssh/core/sec/KeyStoreController.java
@@ -1,8 +1,13 @@
 package de.unipassau.isl.evs.ssh.core.sec;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 
 import de.ncoder.typedmap.Key;
 import de.unipassau.isl.evs.ssh.core.container.AbstractComponent;
+
+import static de.unipassau.isl.evs.ssh.core.util.UtilityFunctions.byteArrayToHexString;
 
 /**
  * The KeyStoreController controls the KeyStore which can load and store keys and provides Key generation.
@@ -54,4 +59,15 @@ public class KeyStoreController extends AbstractComponent {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Calculates the fingerprint of a public key.
+     * @param publicKey PublicKey to calculate fingerprint for.
+     * @return Fingerprint as String.
+     * @throws NoSuchAlgorithmException
+     */
+    public String calculatePublicKeyFingerprint(PublicKey publicKey)
+            throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        return byteArrayToHexString(md.digest(publicKey.getEncoded()));
+    }
 }
