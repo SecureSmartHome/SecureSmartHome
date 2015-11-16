7d6d3ebb2affda77f2cdfaa75b78f1dc986eea98
Niko Fink <nfink95@gmail.com>
2015-11-16 11:42:13 +0100

    Added Test case foe SimpleContainer

---
+++ core/src/androidTest/java/de/unipassau/isl/evs/ssh/core/container/SimpleContainerTest.java
@@ -0,0 +1,113 @@
+package de.unipassau.isl.evs.ssh.core.container;
+
+import junit.framework.TestCase;
+
+import de.ncoder.typedmap.Key;
+
+public class SimpleContainerTest extends TestCase {
+    private final Key<TestComponent> k = new Key<>(TestComponent.class);
+    private Container container = new SimpleContainer();
+    private Key<TestComponent> k1 = new Key<>(TestComponent.class, "1");
+    private Key<TestComponent> k2 = new Key<>(TestComponent.class, "2");
+    private TestComponent c1 = new TestComponent();
+    private TestComponent c2 = new TestComponent();
+
+    public void testRegister() {
+        assertFalse(c1.isActive());
+        assertFalse(c2.isActive());
+        assertNull(container.get(k1));
+        assertNull(container.get(k2));
+        assertNull(container.get(k));
+
+        container.register(k1, c1);
+
+        assertTrue(c1.isActive());
+        assertFalse(c2.isActive());
+        assertEquals(container.get(k1), c1);
+        assertNull(container.get(k2));
+        assertNull(container.get(k));
+
+        container.register(k2, c2);
+
+        assertTrue(c1.isActive());
+        assertTrue(c2.isActive());
+        assertEquals(container.get(k1), c1);
+        assertEquals(container.get(k2), c2);
+        assertEquals(container.require(k1), c1);
+        assertEquals(container.require(k2), c2);
+        assertNull(container.get(k));
+
+        try {
+            container.register(k1, c2);
+            fail("registered a component for an already registered key");
+        } catch (Container.ComponentException e) {
+        }
+
+        assertEquals(c1.initCount, 1);
+        assertEquals(c2.initCount, 1);
+
+        container.unregister(k1);
+
+        assertFalse(c1.isActive());
+        assertTrue(c2.isActive());
+        assertNull(container.get(k1));
+        assertEquals(container.get(k2), c2);
+        assertEquals(c1.destroyCount, 1);
+        assertEquals(c2.destroyCount, 0);
+
+        container.unregister(c2);
+
+        assertFalse(c1.isActive());
+        assertFalse(c2.isActive());
+        assertNull(container.get(k1));
+        assertNull(container.get(k2));
+        assertEquals(c1.destroyCount, 1);
+        assertEquals(c2.destroyCount, 1);
+
+        try {
+            container.require(k1);
+            fail("missing required dependency");
+        } catch (Container.ComponentException e) {
+        }
+        try {
+            container.require(k2);
+            fail("missing required dependency");
+        } catch (Container.ComponentException e) {
+        }
+    }
+
+    public void testShutdown() {
+        container.register(k1, c1);
+        container.register(k2, c2);
+
+        assertTrue(c1.isActive());
+        assertTrue(c2.isActive());
+
+        container.shutdown();
+
+        assertFalse(c1.isActive());
+        assertFalse(c2.isActive());
+        assertNull(container.get(k1));
+        assertNull(container.get(k2));
+        assertEquals(c1.destroyCount, 1);
+        assertEquals(c2.destroyCount, 1);
+    }
+
+
+    private class TestComponent extends AbstractComponent {
+        private int initCount;
+        private int destroyCount;
+
+        @Override
+        public void init(Container container) {
+            super.init(container);
+            initCount++;
+        }
+
+        @Override
+        public void destroy() {
+            super.destroy();
+            destroyCount++;
+        }
+    }
+}
