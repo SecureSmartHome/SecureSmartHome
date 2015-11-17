a60c009de2cee4e2dff2306d1f0063e17c8186f0
Niko Fink <nfink95@gmail.com>
2015-11-17 15:33:52 +0100

    Added constants for Server

--- core/src/main/java/de/unipassau/isl/evs/ssh/core/CoreConstants.java
+++ core/src/main/java/de/unipassau/isl/evs/ssh/core/CoreConstants.java
@@ -4,4 +4,6 @@ package de.unipassau.isl.evs.ssh.core;
  * This Constants class provides constants needed by all modules.
  */
 public class CoreConstants {
+    public static final String FILE_SHARED_PREFS = "shared-preferences";
+    public static final int DEFAULT_PORT = 13131;
 }
--- master/src/main/java/de/unipassau/isl/evs/ssh/master/MasterConstants.java
+++ master/src/main/java/de/unipassau/isl/evs/ssh/master/MasterConstants.java
@@ -5,5 +5,6 @@ import de.unipassau.isl.evs.ssh.drivers.DriverConstants;
 /**
  * This Constants class provides constants needed by the master module.
  */
-public class MasterConstants extends DriverConstants{
+public class MasterConstants extends DriverConstants {
+    public static final String PREF_SERVER_PORT = "PREF_SERVER_PORT";
 }
