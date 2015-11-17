c71197a85236dfd0175c930f21ab91984c1b2d96
Wolfgang Popp <poppwolf@fim.uni-passau.de>
2015-11-17 21:04:45 +0100

    added testcase for db connector

---
+++ master/src/androidTest/java/de/unipassau/isl/evs/ssh/master/database/DatabaseConnectorTest.java
@@ -0,0 +1,30 @@
+package de.unipassau.isl.evs.ssh.master.database;
+
+import android.database.Cursor;
+import android.test.InstrumentationTestCase;
+
+import junit.framework.TestCase;
+
+import de.unipassau.isl.evs.ssh.core.container.ContainerService;
+import de.unipassau.isl.evs.ssh.core.container.SimpleContainer;
+
+public class DatabaseConnectorTest extends InstrumentationTestCase {
+
+    public void testExecuteSql() throws Exception {
+        SimpleContainer container = new SimpleContainer();
+        container.register(ContainerService.KEY_CONTEXT,
+                new ContainerService.ContextComponent(getInstrumentation().getTargetContext()));
+
+        container.register(DatabaseConnector.KEY, new DatabaseConnector());
+
+        DatabaseConnector db = container.require(DatabaseConnector.KEY);
+        Cursor c = db.executeSql("select name from sqlite_master where type = 'table' and name = 'HolidayLog'", null);
+        c.moveToFirst();
+        assertEquals(c.getString(c.getColumnIndex("name")), "HolidayLog");
+
+        db.executeSql("insert into UserDevice values (?,?,?);", new String[]{ "1", "bob", "fingerprint" });
+        c = db.executeSql("select * from UserDevice where _ID = 1;", null);
+        c.moveToFirst();
+        assertEquals(c.getString(c.getColumnIndex("name")), "bob");
+    }
+}
