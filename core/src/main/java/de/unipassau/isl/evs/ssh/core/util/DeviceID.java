846cdb86da5b750313596bb6211401d177d9b772
Leon Sell <sellleon@fim.uni-passau.de>
2015-11-17 21:00:04 +0100

    made DeviceID a bean

--- core/src/main/java/de/unipassau/isl/evs/ssh/core/util/DeviceID.java
+++ core/src/main/java/de/unipassau/isl/evs/ssh/core/util/DeviceID.java
@@ -4,7 +4,20 @@ package de.unipassau.isl.evs.ssh.core.util;
  * Unique id for all devices (user devices, master, slaves).
  */
 public class DeviceID {
-
     private String fingerprint;
 
+    public DeviceID() {
+    }
+
+    public DeviceID(String fingerprint) {
+        this.fingerprint = fingerprint;
+    }
+
+    public String getFingerprint() {
+        return fingerprint;
+    }
+
+    public void setFingerprint(String fingerprint) {
+        this.fingerprint = fingerprint;
+    }
 }
