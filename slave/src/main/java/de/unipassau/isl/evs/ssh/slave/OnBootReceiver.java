746bfb1e3e8e075cb5f5f479ab3ed33000eb6809
bucher06 <AndreasBucher28@googlemail.com>
2015-11-18 17:24:47 +0100

    removed author in OnBootReceiver

--- slave/src/main/java/de/unipassau/isl/evs/ssh/slave/OnBootReceiver.java
+++ slave/src/main/java/de/unipassau/isl/evs/ssh/slave/OnBootReceiver.java
@@ -4,9 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-/**
- * Created by bucher on 18.11.15.
- */
 public class OnBootReceiver extends BroadcastReceiver {
 
     @Override
