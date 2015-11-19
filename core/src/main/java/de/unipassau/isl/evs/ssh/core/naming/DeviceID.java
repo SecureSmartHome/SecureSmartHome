beb2cdf66d4b1d1b1f1659d17df84deb1d00d953
Wolfgang Popp <poppwolf@fim.uni-passau.de>
2015-11-19 21:25:55 +0100

    implemented naming manager. still untested

--- core/src/main/java/de/unipassau/isl/evs/ssh/core/naming/DeviceID.java
+++ core/src/main/java/de/unipassau/isl/evs/ssh/core/naming/DeviceID.java
@@ -7,9 +7,10 @@ public class DeviceID {
 
     private String id;
 
-    public DeviceID(String id){
+    public DeviceID(String id) {
         this.id = id;
     }
+
 
     public String getId() {
         return id;
@@ -17,6 +18,30 @@ public class DeviceID {
 
     @Override
     public boolean equals(Object o) {
-        return super.equals(o);
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        DeviceID deviceID = (DeviceID) o;
+
+        if (id != null) {
+            return id.equals(deviceID.id);
+        } else {
+            return deviceID.id == null;
+        }
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "DeviceID{id='" + id + '\'' + '}';
     }
 }
