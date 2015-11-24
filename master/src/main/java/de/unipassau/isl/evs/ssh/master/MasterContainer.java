b11e71a847c829a7063beff9f6b469335cbff9ac
Niko Fink <nfink95@gmail.com>
2015-11-24 16:51:46 +0100

    Add required Components to Containers

--- master/src/main/java/de/unipassau/isl/evs/ssh/master/MasterContainer.java
+++ master/src/main/java/de/unipassau/isl/evs/ssh/master/MasterContainer.java
@@ -1,18 +1,22 @@
 package de.unipassau.isl.evs.ssh.master;
 
 import de.unipassau.isl.evs.ssh.core.container.ContainerService;
+import de.unipassau.isl.evs.ssh.core.messaging.OutgoingRouter;
+import de.unipassau.isl.evs.ssh.core.naming.NamingManager;
+import de.unipassau.isl.evs.ssh.core.sec.KeyStoreController;
 import de.unipassau.isl.evs.ssh.master.database.DatabaseConnector;
+import de.unipassau.isl.evs.ssh.master.network.Server;
+import de.unipassau.isl.evs.ssh.master.network.ServerOutgoingRouter;
 
 /**
  * This Container class manages dependencies needed in the Master part of the architecture.
  */
 public class MasterContainer extends ContainerService {
-
-    public MasterContainer() {
-    }
-
     @Override
     protected void init() {
         register(DatabaseConnector.KEY, new DatabaseConnector());
+        register(KeyStoreController.KEY, new KeyStoreController());
+        register(NamingManager.KEY, new NamingManager(true));
+        register(Server.KEY, new Server());
     }
 }
