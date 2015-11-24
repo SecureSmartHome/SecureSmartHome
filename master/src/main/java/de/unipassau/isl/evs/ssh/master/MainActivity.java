3f66e731caca452bdb5b3b1fdb1536b0ebe71331
Niko Fink <nfink95@gmail.com>
2015-11-24 18:31:43 +0100

    Fixed logging multiplication due to Handler not being unregistered

--- master/src/main/java/de/unipassau/isl/evs/ssh/master/MainActivity.java
+++ master/src/main/java/de/unipassau/isl/evs/ssh/master/MainActivity.java
@@ -2,6 +2,7 @@ package de.unipassau.isl.evs.ssh.master;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,6 +20,28 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 
 public class MainActivity extends BoundActivity {
+    private final MessageHandler handler = new MessageHandler() {
+        @Override
+        public void handle(final Message.AddressedMessage message) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    log("IN: " + message.toString());
+                }
+            });
+        }
+
+        @Override
+        public void handlerAdded(IncomingDispatcher dispatcher, String routingKey) {
+
+        }
+
+        @Override
+        public void handlerRemoved(String routingKey) {
+
+        }
+    };
+
     public MainActivity() {
         super(MasterContainer.class);
     }
@@ -36,7 +59,7 @@ public class MainActivity extends BoundActivity {
             public void onClick(View v) {
                 Message message = new Message();
                 ChannelFuture future = requireComponent(OutgoingRouter.KEY).sendMessageToMaster("/demo", message);
-                ((TextView) findViewById(R.id.textViewLog)).append("OUT:" + message.toString());
+                log("OUT:" + message.toString());
                 future.addListener(new ChannelFutureListener() {
                     @Override
                     public void operationComplete(final ChannelFuture future) throws Exception {
@@ -67,27 +90,17 @@ public class MainActivity extends BoundActivity {
                 + (server.isExecutorAlive() ? "alive" : "dead") + "]";
         ((TextView) findViewById(R.id.textViewConnectionStatus)).setText(status);
 
-        requireComponent(IncomingDispatcher.KEY).registerHandler(new MessageHandler() {
-            @Override
-            public void handle(final Message.AddressedMessage message) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ((TextView) findViewById(R.id.textViewLog)).append("IN: " + message.toString());
-                    }
-                });
-            }
+        requireComponent(IncomingDispatcher.KEY).registerHandler(handler, "/demo");
+    }
 
-            @Override
-            public void handlerAdded(IncomingDispatcher dispatcher, String routingKey) {
+    @Override
+    public void onContainerDisconnected() {
+        requireComponent(IncomingDispatcher.KEY).unregisterHandler(handler, "/demo");
+    }
 
-            }
-
-            @Override
-            public void handlerRemoved(String routingKey) {
-
-            }
-        }, "/demo");
+    private void log(String text) {
+        Log.v("MASTER", text);
+        ((TextView) findViewById(R.id.textViewLog)).append(text);
     }
 
     @Override
--- slave/src/main/java/de/unipassau/isl/evs/ssh/slave/MainActivity.java
+++ slave/src/main/java/de/unipassau/isl/evs/ssh/slave/MainActivity.java
@@ -2,7 +2,7 @@ package de.unipassau.isl.evs.ssh.slave;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +20,26 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 
 public class MainActivity extends BoundActivity {
+    private final MessageHandler handler = new MessageHandler() {
+        @Override
+        public void handle(final Message.AddressedMessage message) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    log("IN: " + message.toString());
+                }
+            });
+        }
+
+        @Override
+        public void handlerAdded(IncomingDispatcher dispatcher, String routingKey) {
+        }
+
+        @Override
+        public void handlerRemoved(String routingKey) {
+        }
+    };
+
     public MainActivity() {
         super(SlaveContainer.class);
     }
@@ -37,7 +57,7 @@ public class MainActivity extends BoundActivity {
             public void onClick(View v) {
                 Message message = new Message();
                 ChannelFuture future = requireComponent(OutgoingRouter.KEY).sendMessageToMaster("/demo", message);
-                ((TextView) findViewById(R.id.textViewLog)).append("OUT:" + message.toString());
+                log("OUT:" + message.toString());
                 future.addListener(new ChannelFutureListener() {
                     @Override
                     public void operationComplete(final ChannelFuture future) throws Exception {
@@ -64,31 +84,21 @@ public class MainActivity extends BoundActivity {
         ((TextView) findViewById(R.id.textViewComponents)).setText(container.getData().toString());
         Client client = getComponent(Client.KEY);
         String status = "connected to " + client.getAddress() + " "
-                + "[" + (client.isChannelOpen() ? "open" : "closed") + ", "
+                + "[" + (client.isTCPChannelOpen() ? "open" : "closed") + ", "
                 + (client.isExecutorAlive() ? "alive" : "dead") + "]";
         ((TextView) findViewById(R.id.textViewConnectionStatus)).setText(status);
 
-        requireComponent(IncomingDispatcher.KEY).registerHandler(new MessageHandler() {
-            @Override
-            public void handle(final Message.AddressedMessage message) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ((TextView) findViewById(R.id.textViewLog)).append("IN: " + message.toString());
-                    }
-                });
-            }
+        requireComponent(IncomingDispatcher.KEY).registerHandler(handler, "/demo");
+    }
 
-            @Override
-            public void handlerAdded(IncomingDispatcher dispatcher, String routingKey) {
+    @Override
+    public void onContainerDisconnected() {
+        requireComponent(IncomingDispatcher.KEY).unregisterHandler(handler, "/demo");
+    }
 
-            }
-
-            @Override
-            public void handlerRemoved(String routingKey) {
-
-            }
-        }, "/demo");
+    private void log(String text) {
+        Log.v("SLAVE", text);
+        ((TextView) findViewById(R.id.textViewLog)).append(text);
     }
 
     @Override
