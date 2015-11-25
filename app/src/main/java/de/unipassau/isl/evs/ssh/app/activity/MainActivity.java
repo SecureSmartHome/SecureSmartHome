6ef8a84cdfe85a01c578b81ba499c35547218c91
Wolfgang Popp <poppwolf@fim.uni-passau.de>
2015-11-25 20:55:30 +0100

    refactored MainActivity and MainFragment (moved reoccurring code to method)

--- app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/MainActivity.java
+++ app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/MainActivity.java
@@ -2,6 +2,7 @@ package de.unipassau.isl.evs.ssh.app.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -31,18 +32,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         //Set the fragment initially
 
-        MainFragment fragment;
-        if (savedInstanceState != null && savedInstanceState.containsKey(SAVED_LAST_ACTIVE_FRAGMENT)) {
-            int id = savedInstanceState.getInt(SAVED_LAST_ACTIVE_FRAGMENT);
-            showFragmentByID(id);
-        } else {
-            fragment = new MainFragment();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.commit();
-        }
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -54,6 +43,15 @@ public class MainActivity extends AppCompatActivity
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null || !savedInstanceState.containsKey(SAVED_LAST_ACTIVE_FRAGMENT)) {
+            MainFragment fragment = new MainFragment();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        }
+
     }
 
     @Override
@@ -92,84 +90,43 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        int fragmentId = getSupportFragmentManager().getBackStackEntryAt(0).getId();
-        outState.putInt(SAVED_LAST_ACTIVE_FRAGMENT, fragmentId);
+        outState.putBoolean(SAVED_LAST_ACTIVE_FRAGMENT, true);
     }
 
-    private void showFragmentByID(int id){
+    /**
+     * Displays a fragment and takes care of livecycle actions like saving state when rotating the
+     * screen or managing the back button behavior.
+     *
+     * @param id the resource id of the fragment
+     */
+    public void showFragmentByID(int id) {
+        Fragment fragment;
         if (id == R.id.nav_home) {
-            //Set the fragment initially
-            MainFragment fragment = new MainFragment();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
+            fragment = new MainFragment();
         } else if (id == R.id.nav_door) {
-            //Set the fragment initially
-            DoorFragment fragment = new DoorFragment();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
+            fragment = new DoorFragment();
         } else if (id == R.id.nav_light) {
-            //Set the fragment initially
-            LightFragment fragment = new LightFragment();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
+            fragment = new LightFragment();
         } else if (id == R.id.nav_climate) {
-            //Set the fragment initially
-            ClimateFragment fragment = new ClimateFragment();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
+            fragment = new ClimateFragment();
         } else if (id == R.id.nav_holiday) {
-            //Set the fragment initially
-            HolidayFragment fragment = new HolidayFragment();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
+            fragment = new HolidayFragment();
         } else if (id == R.id.nav_status) {
-            //Set the fragment initially
-            StatusFragment fragment = new StatusFragment();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
+            fragment = new StatusFragment();
         } else if (id == R.id.nav_modifyPermissions) {
-            //Set the fragment initially
-            ModifyPermissionFragment fragment = new ModifyPermissionFragment();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
+            fragment = new ModifyPermissionFragment();
         } else if (id == R.id.nav_addNewUserDevice) {
-            //Set the fragment initially
-            AddNewUserDeviceFragment fragment = new AddNewUserDeviceFragment();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
+            fragment = new AddNewUserDeviceFragment();
         } else if (id == R.id.nav_addModul) {
-            //Set the fragment initially
-            AddModulFragment fragment = new AddModulFragment();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
+            fragment = new AddModulFragment();
+        } else {
+            throw new IllegalArgumentException("Unknown id: " + id);
         }
+        android.support.v4.app.FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     @Override
