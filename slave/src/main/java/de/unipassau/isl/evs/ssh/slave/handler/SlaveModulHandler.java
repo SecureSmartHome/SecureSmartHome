c91085c3dbcce4a86385c6e4de726f90c5bc52ed
bucher06 <AndreasBucher28@googlemail.com>
2015-11-26 15:55:16 +0100

    added comments

--- slave/src/main/java/de/unipassau/isl/evs/ssh/slave/handler/SlaveModulHandler.java
+++ slave/src/main/java/de/unipassau/isl/evs/ssh/slave/handler/SlaveModulHandler.java
@@ -1,13 +1,9 @@
 package de.unipassau.isl.evs.ssh.slave.handler;
 
-import com.google.common.base.Predicate;
-
 import java.util.List;
-import java.util.Objects;
 
 import de.unipassau.isl.evs.ssh.core.container.Container;
 import de.unipassau.isl.evs.ssh.core.database.dto.Module;
-import de.unipassau.isl.evs.ssh.core.database.dto.ModuleAccessPoint.GPIOAccessPoint;
 import de.unipassau.isl.evs.ssh.core.handler.MessageHandler;
 import de.unipassau.isl.evs.ssh.core.messaging.IncomingDispatcher;
 import de.unipassau.isl.evs.ssh.core.messaging.Message;
@@ -15,27 +11,24 @@ import de.unipassau.isl.evs.ssh.core.messaging.OutgoingRouter;
 
 /**
  * SlaveModulHandler offers a list of all Modules that are active in the System.
+ *
  * @author bucher
  */
-public class SlaveModulHandler implements MessageHandler{
-    public static final Predicate<Module> COMPONENTS_IN_ODROID = new Predicate<Module>() {
-        @Override
-        public boolean apply(Module input) {
-            return false;
-            //NamingManager
-            //Objects.equals(input.getAtSlave().getId(), require("KeyStoreManager").getLocalID());
-        }
-    };
+public class SlaveModulHandler implements MessageHandler {
 
     private OutgoingRouter outgoing;
     private Container container;
     private IncomingDispatcher incomingDispatcher;
     private List<Module> components;
 
-    public void UpdateModule(List<Module> components){
+    public void UpdateModule(List<Module> components) {
         this.components = components;
-
     }
+
+    public List<Module> getComponents() {
+        return components;
+    }
+
     @Override
     public void handle(Message.AddressedMessage message) {
     }
