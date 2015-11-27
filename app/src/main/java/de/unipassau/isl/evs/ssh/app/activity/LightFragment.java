cbf3db318dbef1b007ebc1335e404138a61fd3e2
Phil Werli <werli@fim.uni-passau.de>
2015-11-27 04:48:40 +0100

    edit lightfragment, so the registered lights are actually displayed. uses a listview now.

--- app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/LightFragment.java
+++ app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/LightFragment.java
@@ -1,17 +1,24 @@
 package de.unipassau.isl.evs.ssh.app.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 
 import de.unipassau.isl.evs.ssh.app.R;
@@ -33,27 +40,115 @@ public class LightFragment extends Fragment {
     public LightFragment() {
         // Required empty public constructor
     }
-
-
+   @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       FrameLayout root = (FrameLayout) inflater.inflate(R.layout.fragment_light, container, false);
+       ListView list = (ListView) root.findViewById(R.id.lightButtonContainer);
+
+
+       final Map<Module, Boolean> lightModulesStatus = getLightModules();
+
+       final ArrayList<Module> lightModules = Lists.newArrayList(lightModulesStatus.keySet());
+       Collections.sort(lightModules, new Comparator<Module>() {
+           @Override
+           public int compare(Module lhs, Module rhs) {
+               if(lhs.getName() == null) {
+                   return  rhs.getName() == null ? 0: 1;
+               }
+               if(rhs.getName() == null) {
+                   return  -1;
+               }
+               return lhs.getName().compareTo(rhs.getName());
+           }
+       });
+
+       list.setAdapter(new BaseAdapter() {
+           @Override
+           public int getCount() {
+               return lightModules.size();
+           }
+
+           @Override
+           public Module getItem(int position) {
+               return lightModules.get(position);
+           }
+
+           @Override
+           public long getItemId(int position) {
+               return getItem(position).getName().hashCode(); // TODO nullcheck
+           }
+
+           @Override
+           public boolean hasStableIds() {
+               return true;
+           }
+
+           @Override
+           public View getView(int position, View convertView, ViewGroup parent) {
+               //create linearlayout as defined in lightbutton.xml file
+               LinearLayout lightButtonLayout;
+               if(convertView == null) {
+                   lightButtonLayout = (LinearLayout) inflater.inflate(R.layout.lightbutton, null, false);
+               } else {
+                   lightButtonLayout = (LinearLayout) convertView;
+               }
+
+               final Module m = getItem(position);
+
+               TextView textView = (TextView) lightButtonLayout.findViewById(R.id.lightButtonTextView);
+               Button button = (Button) lightButtonLayout.findViewById(R.id.lightButtonButton);
+               button.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                       ((MainActivity) getActivity()).getContainer().require(AppLightHandler.KEY).toggleLight(m);
+                   }
+               });
+               // set up TextView
+               char[] name = getName(m);
+               textView.setText(name, 0, name.length);
+
+               // set up ImageView and button
+               ImageView imageViewOn = (ImageView) lightButtonLayout.findViewById(R.id.lightButtonImageViewOn);
+               ImageView imageViewOff = (ImageView) lightButtonLayout.findViewById(R.id.lightButtonImageViewOff);
+               if (lightModulesStatus.get(m)) {
+                   imageViewOff.setVisibility(View.GONE);
+                   imageViewOn.setVisibility(View.VISIBLE);
+                   button.setText(getResources().getString(R.string.string_turn_off));
+                   button.setBackgroundColor(getResources().getColor(R.color.button_red));
+               } else {
+                   imageViewOn.setVisibility(View.GONE);
+                   imageViewOff.setVisibility(View.VISIBLE);
+                   button.setText(getResources().getString(R.string.string_turn_on));
+                   button.setBackgroundColor(getResources().getColor(R.color.button_green));
+               }
+
+               return lightButtonLayout;
+           }
+       });
+
+       return root;
+    }
+
+
+ /*public View onCreateView2(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         FrameLayout mLinearLayout = (FrameLayout) inflater.inflate(R.layout.fragment_light,
                 container, false);
-        LinearLayout lightButtonLayout = (LinearLayout) mLinearLayout.findViewById(R.id.lightButtonLayout);
+        LinearLayout lightButtonContainer = (LinearLayout) mLinearLayout.findViewById(R.id.lightButtonContainerLayout);
 
         Map<Module, Boolean> lightModules = getLightModules();
         for (final Module m : lightModules.keySet()) {
             //create linearlayout as defined in lightbutton.xml file
-            LinearLayout lightButtonContainer = (LinearLayout) inflater.inflate(R.layout.lightbutton, container, false);
+            LinearLayout lightButtonLayout = (LinearLayout) inflater.inflate(R.layout.lightbutton, container, false);
 
-            TextView textView = (TextView) lightButtonContainer.findViewById(R.id.lightButtonTextView);
-            Button button = (Button) lightButtonContainer.findViewById(R.id.lightButtonButton);
+            TextView textView = (TextView) lightButtonLayout.findViewById(R.id.lightButtonTextView);
+            Button button = (Button) lightButtonLayout.findViewById(R.id.lightButtonButton);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    ((MainActivity) getActivity()).getContainer().require(AppLightHandler.KEY).toggleLight(m);
+                    (A(MainActivity) getActivity()).getContainer().require(AppLightHandler.KEY).toggleLight(m);
                 }
             });
             // set up TextView
@@ -61,8 +156,8 @@ public class LightFragment extends Fragment {
             textView.setText(name, 0, name.length);
 
             // set up ImageView and button
-            ImageView imageViewOn = (ImageView) lightButtonContainer.findViewById(R.id.lightButtonImageViewOn);
-            ImageView imageViewOff = (ImageView) lightButtonContainer.findViewById(R.id.lightButtonImageViewOff);
+            ImageView imageViewOn = (ImageView) lightButtonLayout.findViewById(R.id.lightButtonImageViewOn);
+            ImageView imageViewOff = (ImageView) lightButtonLayout.findViewById(R.id.lightButtonImageViewOff);
             if (lightModules.get(m)) {
                 imageViewOff.setVisibility(View.GONE);
                 imageViewOn.setVisibility(View.VISIBLE);
@@ -74,10 +169,7 @@ public class LightFragment extends Fragment {
                 button.setText(getResources().getString(R.string.string_turn_on));
                 button.setBackgroundColor(getResources().getColor(R.color.button_green));
             }
-            lightButtonLayout.addView(textView);
-            lightButtonLayout.addView(button);
-            lightButtonLayout.addView(imageViewOff);
-            lightButtonLayout.addView(imageViewOn);
+
             lightButtonContainer.addView(lightButtonLayout);
         }
 
@@ -92,8 +184,8 @@ public class LightFragment extends Fragment {
                 //fixme call add light module fragment
             }
         });
-        return inflater.inflate(R.layout.fragment_light, container, false);
-    }
+        return mLinearLayout;
+    }*/
 
     @Override
     public void onDestroyView() {
