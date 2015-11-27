00ad4a7d45906790180cdc0bc05717e8d7eef466
Niko Fink <nfink95@gmail.com>
2015-11-27 02:16:01 +0100

    Fixed TimeoutHandler inadvertently closing connections

--- core/src/main/java/de/unipassau/isl/evs/ssh/core/network/handler/TimeoutHandler.java
+++ core/src/main/java/de/unipassau/isl/evs/ssh/core/network/handler/TimeoutHandler.java
@@ -1,11 +1,14 @@
 package de.unipassau.isl.evs.ssh.core.network.handler;
 
+import android.util.Log;
+
 import java.io.Serializable;
 
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.util.ReferenceCountUtil;
 
 /**
  * TimeoutHandler class is part of the netty pipeline. Handles timeouts by sending {@link PingMessage}.
@@ -13,6 +16,8 @@ import io.netty.handler.timeout.IdleStateEvent;
  * @author Phil
  */
 public class TimeoutHandler extends ChannelHandlerAdapter {
+    private static final String TAG = TimeoutHandler.class.getSimpleName();
+
     /**
      * Handles received timeout event.
      * Sends a {@link PingMessage}.
@@ -22,6 +27,7 @@ public class TimeoutHandler extends ChannelHandlerAdapter {
         if (evt instanceof IdleStateEvent) {
             IdleStateEvent e = (IdleStateEvent) evt;
             if (e.state() == IdleState.READER_IDLE) {
+                Log.d(TAG, "Connection timed out");
                 ctx.close();
             } else if (e.state() == IdleState.WRITER_IDLE) {
                 ctx.writeAndFlush(new PingMessage());
@@ -36,16 +42,18 @@ public class TimeoutHandler extends ChannelHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (!(msg instanceof PingMessage)) {
+        if (msg instanceof PingMessage) {
+            // discard message
+            ReferenceCountUtil.release(msg);
+        } else {
+            // forward message
             super.channelRead(ctx, msg);
-        } // else discard message
-        ctx.close();
+        }
     }
 
     /**
      * Empty class used for simple ping messages.
      */
     public static class PingMessage implements Serializable {
-
     }
 }
