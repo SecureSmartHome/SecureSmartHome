33e3104c715b41361dfdd8a7a511e3ad9d257b7b
Wolfgang Popp <poppwolf@fim.uni-passau.de>
2015-12-02 12:58:53 +0100

    started working on fragment to add userdevices

--- app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/AddNewUserDeviceFragment.java
+++ app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/AddNewUserDeviceFragment.java
@@ -5,8 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import de.unipassau.isl.evs.ssh.app.AppModuleHandler;
 import de.unipassau.isl.evs.ssh.app.R;
+import de.unipassau.isl.evs.ssh.core.database.dto.Group;
 import de.unipassau.isl.evs.ssh.core.handler.MessageHandler;
 import de.unipassau.isl.evs.ssh.core.messaging.IncomingDispatcher;
 import de.unipassau.isl.evs.ssh.core.messaging.Message;
@@ -18,16 +26,22 @@ import de.unipassau.isl.evs.ssh.core.messaging.Message;
  */
 public class AddNewUserDeviceFragment extends Fragment implements MessageHandler {
 
-    public AddNewUserDeviceFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_addnewuserdevice, container, false);
+
+        // TODO get groups
+        List<String> groups = new LinkedList<>();
+        groups.add("Eltern");
+        groups.add("Kinder");
+        groups.add("Gäste");
+        View view = inflater.inflate(R.layout.fragment_addnewuserdevice, container, false);
+        Spinner spinner = (Spinner) view.findViewById(R.id.groupSpinner);
+        SpinnerAdapter adapter = new ArrayAdapter<>(getActivity().getApplicationContext(),
+                android.R.layout.simple_list_item_1, groups);
+        spinner.setAdapter(adapter);
+        return view;
     }
 
     @Override
@@ -44,4 +58,5 @@ public class AddNewUserDeviceFragment extends Fragment implements MessageHandler
     public void handlerRemoved(String routingKey) {
 
     }
+
 }
