320996a27ac554d683bd6c069da04d114b6a840c
Niko Fink <nfink95@gmail.com>
2015-12-02 17:49:43 +0100

    Add callbacks for fragments when the Container is bound

--- app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/MainActivity.java
+++ app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import de.unipassau.isl.evs.ssh.app.AppContainer;
 import de.unipassau.isl.evs.ssh.app.R;
 import de.unipassau.isl.evs.ssh.core.activity.BoundActivity;
+import de.unipassau.isl.evs.ssh.core.container.Container;
 
 /**
  * As this Activity also displays information like wether the light is on or not, this Activity also
@@ -107,7 +108,7 @@ public class MainActivity extends BoundActivity
     }
 
     // returns the currently displayed Fragment
-    private Fragment getCurrentFragment(){
+    private Fragment getCurrentFragment() {
         return getSupportFragmentManager().findFragmentById(R.id.fragment_container);
     }
 
@@ -117,7 +118,7 @@ public class MainActivity extends BoundActivity
      *
      * @param clazz the class of the fragment to show
      */
-    public void showFragmentByClass(Class clazz){
+    public void showFragmentByClass(Class clazz) {
         Class oldFragment = getCurrentFragment().getClass();
         Fragment fragment = null;
         try {
@@ -172,5 +173,21 @@ public class MainActivity extends BoundActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onContainerConnected(Container container) {
+        final Fragment fragment = getCurrentFragment();
+        if (fragment instanceof BoundFragment) {
+            ((BoundFragment) fragment).onContainerConnected(container);
+        }
+    }
+
+    @Override
+    public void onContainerDisconnected() {
+        final Fragment fragment = getCurrentFragment();
+        if (fragment instanceof BoundFragment) {
+            ((BoundFragment) fragment).onContainerDisconnected();
+        }
     }
 }
