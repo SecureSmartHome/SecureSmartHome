3e810966015727142aa819b100679f0bda7417db
Leon Sell <sellleon@fim.uni-passau.de>
2015-12-07 19:20:55 +0100

    first device now gets all permissions

--- master/src/main/java/de/unipassau/isl/evs/ssh/master/handler/MasterRoutingTableHandler.java
+++ master/src/main/java/de/unipassau/isl/evs/ssh/master/handler/MasterRoutingTableHandler.java
@@ -1,7 +1,5 @@
 package de.unipassau.isl.evs.ssh.master.handler;
 
-import de.unipassau.isl.evs.ssh.core.handler.MessageHandler;
-import de.unipassau.isl.evs.ssh.core.messaging.IncomingDispatcher;
 import de.unipassau.isl.evs.ssh.core.messaging.Message;
 
 /**
@@ -9,20 +7,10 @@ import de.unipassau.isl.evs.ssh.core.messaging.Message;
  * <p/>
  * An example when this handler needs to take action is when a new sensor is added or switched to a new GPIO Pin.
  */
-public class MasterRoutingTableHandler implements MessageHandler {
+public class MasterRoutingTableHandler extends AbstractMasterHandler {
 
     @Override
     public void handle(Message.AddressedMessage message) {
-
-    }
-
-    @Override
-    public void handlerAdded(IncomingDispatcher dispatcher, String routingKey) {
-
-    }
-
-    @Override
-    public void handlerRemoved(String routingKey) {
 
     }
 }
