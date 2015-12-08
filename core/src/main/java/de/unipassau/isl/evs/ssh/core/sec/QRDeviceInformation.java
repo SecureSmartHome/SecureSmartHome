52ed17d30c22530f5a882edae9754a0f2e12f9da
Niko Fink <nfink95@gmail.com>
2015-12-08 14:16:41 +0100

    Add toString to QRDeviceInformation and correct imports of Base64 utils

--- core/src/main/java/de/unipassau/isl/evs/ssh/core/sec/QRDeviceInformation.java
+++ core/src/main/java/de/unipassau/isl/evs/ssh/core/sec/QRDeviceInformation.java
@@ -3,6 +3,7 @@ package de.unipassau.isl.evs.ssh.core.sec;
 import android.graphics.Bitmap;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.MoreObjects;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
@@ -18,7 +19,13 @@ import de.unipassau.isl.evs.ssh.core.naming.DeviceID;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.UnpooledByteBufAllocator;
-import io.netty.handler.codec.base64.Base64;
+
+import static android.util.Base64.NO_WRAP;
+import static android.util.Base64.decode;
+import static android.util.Base64.encode;
+import static android.util.Base64.encodeToString;
+import static io.netty.handler.codec.base64.Base64.decode;
+import static io.netty.handler.codec.base64.Base64.encode;
 
 /**
  * @author Niko Fink
@@ -27,8 +34,8 @@ public class QRDeviceInformation implements Serializable {
     private static final QRCodeWriter writer = new QRCodeWriter();
     private static final int ADDRESS_LENGTH = 4;
     public static final int TOKEN_LENGTH = 35;
-    private static final int BASE64_ENCODED_LENGTH = android.util.Base64.encode(new byte[TOKEN_LENGTH], android.util.Base64.NO_WRAP).length;
-    private static final int DATA_LENGTH = 4 + 2 + BASE64_ENCODED_LENGTH + DeviceID.ID_LENGTH;
+    private static final int TOKEN_LENGTH_BASE64 = encode(new byte[TOKEN_LENGTH], NO_WRAP).length;
+    private static final int DATA_LENGTH = 4 + 2 + TOKEN_LENGTH_BASE64 + DeviceID.ID_LENGTH;
 
     private final Inet4Address address;
     private final int port;
@@ -67,7 +74,7 @@ public class QRDeviceInformation implements Serializable {
     public static QRDeviceInformation fromDataString(String data) throws IOException {
         final ByteBuf base64 = UnpooledByteBufAllocator.DEFAULT.heapBuffer(data.length());
         ByteBufUtil.writeAscii(base64, data);
-        final ByteBuf byteBuf = Base64.decode(base64);
+        final ByteBuf byteBuf = decode(base64);
         if (byteBuf.readableBytes() != DATA_LENGTH) {
             throw new IOException("too many bytes encoded");
         }
@@ -79,9 +86,9 @@ public class QRDeviceInformation implements Serializable {
         final byte[] idData = new byte[DeviceID.ID_LENGTH];
         byteBuf.readBytes(idData);
         final DeviceID id = new DeviceID(idData);
-        final byte[] encodedToken = new byte[BASE64_ENCODED_LENGTH];
+        final byte[] encodedToken = new byte[TOKEN_LENGTH_BASE64];
         byteBuf.readBytes(encodedToken);
-        final byte[] token = android.util.Base64.decode(encodedToken, android.util.Base64.NO_WRAP);
+        final byte[] token = decode(encodedToken, NO_WRAP);
 
         return new QRDeviceInformation(address, port, id, token);
     }
@@ -91,8 +98,8 @@ public class QRDeviceInformation implements Serializable {
         byteBuf.writeBytes(address.getAddress());
         byteBuf.writeShort(port);
         byteBuf.writeBytes(id.getIDBytes());
-        byteBuf.writeBytes(android.util.Base64.encode(token, android.util.Base64.NO_WRAP));
-        return Base64.encode(byteBuf).toString(Charsets.US_ASCII);
+        byteBuf.writeBytes(encode(token, NO_WRAP));
+        return encode(byteBuf).toString(Charsets.US_ASCII);
     }
 
     public BitMatrix toQRBitMatrix() throws WriterException {
@@ -126,5 +133,15 @@ public class QRDeviceInformation implements Serializable {
         }
         random.nextBytes(token);
         return token;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("address", address)
+                .add("port", port)
+                .add("id", id)
+                .add("token", encodeToString(token, NO_WRAP))
+                .toString();
     }
 }
