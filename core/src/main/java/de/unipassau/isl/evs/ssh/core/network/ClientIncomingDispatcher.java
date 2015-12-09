12cdb1b8cebd225a9b3beb72ea1e1ef4f20736d5
Niko Fink <nfink95@gmail.com>
2015-12-09 17:34:23 +0100

    Clean-Up

--- core/src/main/java/de/unipassau/isl/evs/ssh/core/network/ClientIncomingDispatcher.java
+++ core/src/main/java/de/unipassau/isl/evs/ssh/core/network/ClientIncomingDispatcher.java
@@ -2,12 +2,9 @@ package de.unipassau.isl.evs.ssh.core.network;
 
 import android.util.Log;
 
-import java.util.Set;
-
-import de.unipassau.isl.evs.ssh.core.handler.MessageHandler;
 import de.unipassau.isl.evs.ssh.core.messaging.IncomingDispatcher;
-import de.unipassau.isl.evs.ssh.core.messaging.Message;
 import io.netty.channel.ChannelHandler;
+import io.netty.channel.EventLoop;
 
 /**
  * Distributes incoming messages from the one single connection a client has to the responsible handlers.
@@ -18,28 +15,18 @@ import io.netty.channel.ChannelHandler;
 public class ClientIncomingDispatcher extends IncomingDispatcher {
     private static final String TAG = ClientIncomingDispatcher.class.getSimpleName();
 
-    /**
-     * Dispatches an AddressedMessage to its target handler using the EventExecutor of the Client.
-     *
-     * @param msg AddressedMessage to dispatch.
-     * @return {@code true} if the Message was forwarded to at least one MessageHandler.
-     */
-    @Override
-    public boolean dispatch(final Message.AddressedMessage msg) {
-        Client client = getContainer().require(Client.KEY);
-        if (!client.isExecutorAlive() || !client.isChannelOpen()) {
-            Log.w(TAG, "Could not dispatch message as Executor was shut down");
-            return false;
+    private EventLoop eventLoop;
+
+    protected EventLoop getExecutor() {
+        if (eventLoop == null || eventLoop.isShuttingDown() || eventLoop.isShutdown()) {
+            Log.v(TAG, "EventLoop unavailable (" + eventLoop + "), getting new one");
+            Client client = getContainer().require(Client.KEY);
+            if (!client.isExecutorAlive() || !client.isChannelOpen()) {
+                Log.w(TAG, "Could not dispatch message as Executor was shut down");
+                return null;
+            }
+            eventLoop = client.getExecutor().next();
         }
-        Set<MessageHandler> handlers = mappings.get(msg.getRoutingKey());
-        for (final MessageHandler handler : handlers) {
-            client.getExecutor().submit(new Runnable() {
-                @Override
-                public void run() {
-                    handler.handle(msg);
-                }
-            });
-        }
-        return !handlers.isEmpty();
+        return eventLoop;
     }
 }
