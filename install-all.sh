ba058c3b7a0a0ef697c617b60d118fff7bc791e3
Niko Fink <nfink95@gmail.com>
2015-12-24 15:33:26 +0100

    Add install-all and uninstall-all scripts

---
+++ install-all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./gradlew master:installDebug app:installDebug slave:installDebug
+adb shell "am start  -n 'de.unipassau.isl.evs.ssh.master/de.unipassau.isl.evs.ssh.master.activity.MainActivity' -a android.intent.action.MAIN -c android.intent.category.LAUNCHER"
