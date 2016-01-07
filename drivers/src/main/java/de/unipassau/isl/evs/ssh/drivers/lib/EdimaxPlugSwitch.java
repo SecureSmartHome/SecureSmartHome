044fdc8e32ef1f0b2062ec17c3d30b7b58c49753
Niko Fink <nfink95@gmail.com>
2016-01-07 15:50:26 +0100

    Add ToDo to EdimaxPlugSwitch

--- drivers/src/main/java/de/unipassau/isl/evs/ssh/drivers/lib/EdimaxPlugSwitch.java
+++ drivers/src/main/java/de/unipassau/isl/evs/ssh/drivers/lib/EdimaxPlugSwitch.java
@@ -142,20 +142,14 @@ public class EdimaxPlugSwitch extends AbstractComponent {
             return response.toString();
         } finally {
             if (connection != null) {
-                connection.disconnect();
+                connection.disconnect(); //TODO Niko: check if keeping the connection open leads to a speedup (Wolfi, 2016-01-06)
             }
         }
     }
 
     private boolean parseResponseSet(String response) throws IOException {
         // Parse the response String
-        final Document document;
-        try {
-            final DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            document = builder.parse(new InputSource(new StringReader(response)));
-        } catch (ParserConfigurationException | SAXException e) {
-            throw new IOException("Could not parse response", e);
-        }
+        final Document document = getDocument(response);
 
         // Extract response status
         Element setup = document.getElementById("setup");
@@ -171,13 +165,7 @@ public class EdimaxPlugSwitch extends AbstractComponent {
 
     private boolean parseResponseGet(String response) throws IOException {
         // Parse the response String
-        final Document document;
-        try {
-            final DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            document = builder.parse(new InputSource(new StringReader(response)));
-        } catch (ParserConfigurationException | SAXException e) {
-            throw new IOException("Could not parse response", e);
-        }
+        final Document document = getDocument(response);
 
         // Extract the status
         final NodeList elements = document.getElementsByTagName("Device.System.Power.State");
@@ -187,6 +175,15 @@ public class EdimaxPlugSwitch extends AbstractComponent {
             return state != null && state.toLowerCase().equals("on");
         } else {
             throw new IOException("Response missing Element with tag 'Device.System.Power.State'");
+        }
+    }
+
+    private Document getDocument(String response) throws IOException {
+        try {
+            final DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            return builder.parse(new InputSource(new StringReader(response)));
+        } catch (ParserConfigurationException | SAXException e) {
+            throw new IOException("Could not parse response", e);
         }
     }
 }
