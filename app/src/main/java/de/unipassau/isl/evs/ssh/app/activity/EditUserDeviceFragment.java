c65a7b7d6c0bdc724e1e209fbf63c0610d02c07e
Phil Werli <werli@fim.uni-passau.de>
2016-01-07 19:18:52 +0100

    add overlay to permission if not granted

--- app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/EditUserDeviceFragment.java
+++ app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/EditUserDeviceFragment.java
@@ -18,6 +18,7 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -267,13 +268,22 @@ public class EditUserDeviceFragment extends BoundFragment {
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = getActivity().getLayoutInflater();
             final Permission permission = getItem(position);
-            LinearLayout permissionLayout;
+            RelativeLayout permissionLayout;
             if (convertView == null) {
-                permissionLayout = (LinearLayout) inflater.inflate(R.layout.permissionlayout, parent, false);
+                permissionLayout = (RelativeLayout) inflater.inflate(R.layout.permissionlayout, parent, false);
             } else {
-                permissionLayout = (LinearLayout) convertView;
+                permissionLayout = (RelativeLayout) convertView;
             }
             // TODO Phil: gray out the permissions a user can't change.
+
+            boolean userHasPermission = userDeviceHasPermission(permission);
+            if (!userHasPermission) {
+                LinearLayout overlay = ((LinearLayout) permissionLayout.findViewById(R.id.overlay_permission));
+                overlay.setVisibility(View.VISIBLE);
+
+                TextView overlay_text = (TextView) permissionLayout.findViewById(R.id.overlay_permission_text);
+                overlay_text.setText(R.string.missing_permission);
+            }
 
             final Switch permissionSwitch = (Switch) permissionLayout.findViewById(R.id.listpermission_permission_switch);
             final AppUserConfigurationHandler handler = getComponent(AppUserConfigurationHandler.KEY);
@@ -281,7 +291,7 @@ public class EditUserDeviceFragment extends BoundFragment {
                 Log.i(TAG, "Container not yet connected!");
             } else {
                 permissionSwitch.setText(permission.getName());
-                permissionSwitch.setChecked(userDeviceHasPermission(permission));
+                permissionSwitch.setChecked(userHasPermission);
                 permissionSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
