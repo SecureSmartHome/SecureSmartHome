b14278d7d4349ce080c8b1c5e10813462899f37b
Christoph Fraedrich <fraedric@fim.uni-passau.de>
2016-01-08 14:38:15 +0100

    MasterUserLocationHandler now provides function to check how long the last change was ago

--- master/src/main/java/de/unipassau/isl/evs/ssh/master/handler/MasterUserLocationHandler.java
+++ master/src/main/java/de/unipassau/isl/evs/ssh/master/handler/MasterUserLocationHandler.java
@@ -1,9 +1,12 @@
 package de.unipassau.isl.evs.ssh.master.handler;
 
+import android.support.annotation.NonNull;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import de.ncoder.typedmap.Key;
 import de.unipassau.isl.evs.ssh.core.container.Component;
@@ -30,7 +33,7 @@ public class MasterUserLocationHandler extends AbstractMasterHandler implements 
 
     //Contains a mapping from boolean to a queue which contains information whether the device
     //connected to local
-    private final Map<DeviceID, LinkedList<Boolean>> positionMap = new HashMap<>();
+    private final Map<DeviceID, LinkedList<Message.AddressedMessage>> positionMap = new HashMap<>();
 
     @Override
     public RoutingKey[] getRoutingKeys() {
@@ -45,11 +48,11 @@ public class MasterUserLocationHandler extends AbstractMasterHandler implements 
             DeviceConnectedPayload payload = RoutingKeys.MASTER_DEVICE_CONNECTED.getPayload(message);
 
             if (!positionMap.containsKey(payload.deviceID)) {
-                positionMap.put(payload.deviceID, new LinkedList<Boolean>());
+                positionMap.put(payload.deviceID, new LinkedList<Message.AddressedMessage>());
             }
 
-            List<Boolean> list = positionMap.get(payload.deviceID);
-            list.add(0, payload.isLocal);
+            List<Message.AddressedMessage> list = positionMap.get(payload.deviceID);
+            list.add(0, message);
 
             if (list.size() > MAX_POSITION_COUNT) {
                 list.remove(MAX_POSITION_COUNT);
@@ -57,7 +60,29 @@ public class MasterUserLocationHandler extends AbstractMasterHandler implements 
         }
     }
 
-    public boolean switchedPositionToLocal(DeviceID deviceID) {
-        return (positionMap.get(deviceID).get(0) && !positionMap.get(deviceID).get(1));
+    /**
+     * Checks if the given Device switched from extern to local within the last 'intervall' minutes
+     *
+     * @param deviceID of the device to be checked
+     * @param intervall in which the change has happened in min
+     *
+     * @return true if change to local happened in the last 'intervall' min.
+     */
+    public boolean switchedPositionToLocal(DeviceID deviceID, int intervall) {
+        if (positionMap.get(deviceID) == null || positionMap.get(deviceID).size() >= 2) {
+            return false;
+        }
+
+        Message.AddressedMessage lastMessage = positionMap.get(deviceID).get(0);
+        Message.AddressedMessage preLastMessage = positionMap.get(deviceID).get(1);
+
+        //Is last location local and was last outside of home network and was that change in the last 2 min?
+        if (MASTER_DEVICE_CONNECTED.getPayload(lastMessage).isLocal
+                && !MASTER_DEVICE_CONNECTED.getPayload(preLastMessage).isLocal
+                && lastMessage.getHeaders().get(Message.HEADER_TIMESTAMP) - System.currentTimeMillis() > TimeUnit.MINUTES.toMillis(intervall)) {
+            return true;
+        }
+
+        return false;
     }
 }
