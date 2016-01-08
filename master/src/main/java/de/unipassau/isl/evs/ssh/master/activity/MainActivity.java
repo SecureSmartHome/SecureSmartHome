d654547db42a48a6449df3b88631eec82d959e1d
Phil Werli <werli@fim.uni-passau.de>
2016-01-08 16:51:53 +0100

    rebuild MainActivity in master. add new layout with some connection information and two ListViews. one for the slaves, one for the connected clients. add menu entries settings and reboot (does reboot master). need to add layout for the ListViews tho [WIP]

--- master/src/main/java/de/unipassau/isl/evs/ssh/master/activity/MainActivity.java
+++ master/src/main/java/de/unipassau/isl/evs/ssh/master/activity/MainActivity.java
@@ -9,39 +9,42 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.google.common.collect.Lists;
 
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 import de.unipassau.isl.evs.ssh.core.CoreConstants;
 import de.unipassau.isl.evs.ssh.core.activity.BoundActivity;
 import de.unipassau.isl.evs.ssh.core.container.Container;
 import de.unipassau.isl.evs.ssh.core.container.ContainerService;
+import de.unipassau.isl.evs.ssh.core.database.dto.Slave;
 import de.unipassau.isl.evs.ssh.core.database.dto.UserDevice;
-import de.unipassau.isl.evs.ssh.core.handler.MessageHandler;
-import de.unipassau.isl.evs.ssh.core.messaging.IncomingDispatcher;
-import de.unipassau.isl.evs.ssh.core.messaging.Message;
-import de.unipassau.isl.evs.ssh.core.messaging.OutgoingRouter;
-import de.unipassau.isl.evs.ssh.core.messaging.RoutingKey;
 import de.unipassau.isl.evs.ssh.core.naming.DeviceID;
 import de.unipassau.isl.evs.ssh.core.naming.NamingManager;
 import de.unipassau.isl.evs.ssh.core.sec.DeviceConnectInformation;
 import de.unipassau.isl.evs.ssh.master.MasterContainer;
 import de.unipassau.isl.evs.ssh.master.R;
+import de.unipassau.isl.evs.ssh.master.database.SlaveController;
 import de.unipassau.isl.evs.ssh.master.database.UserManagementController;
 import de.unipassau.isl.evs.ssh.master.handler.MasterRegisterDeviceHandler;
 import de.unipassau.isl.evs.ssh.master.network.Server;
 import io.netty.channel.Channel;
 import io.netty.channel.group.ChannelGroup;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
 
 /**
  * MainActivity for the Master App
- * <p/>
+ * <p>
  * TODO Phil: build MainActivity for Master. Connection Status, own ID, connected modules and connection information.
  *
  * @author Team
@@ -49,27 +52,8 @@ import io.netty.util.concurrent.GenericFutureListener;
 public class MainActivity extends BoundActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private final MessageHandler handler = new MessageHandler() {
-        @Override
-        public void handle(final Message.AddressedMessage message) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    log("IN: " + message.toString());
-                }
-            });
-        }
-
-        @Override
-        public void handlerAdded(IncomingDispatcher dispatcher, RoutingKey routingKey) {
-
-        }
-
-        @Override
-        public void handlerRemoved(RoutingKey routingKey) {
-
-        }
-    };
+    private ListView slaveList;
+    private ListView userDeviceList;
 
     public MainActivity() {
         super(MasterContainer.class);
@@ -89,32 +73,32 @@ public class MainActivity extends BoundActivity {
      * author Phil Werli
      */
     private boolean hasNoRegisteredDevice() {
-        List<UserDevice> list = getContainer().require(UserManagementController.KEY).getUserDevices();
+        UserManagementController component = getComponent(UserManagementController.KEY);
+        if (component == null) {
+            Log.i(TAG, "Container not yet connected.");
+            return true;
+        }
+        List<UserDevice> list = component.getUserDevices();
         return list.size() == 0;
     }
 
-
     @Override
     public void onContainerConnected(Container container) {
-        final NamingManager namingManager = getComponent(NamingManager.KEY);
-        if (namingManager == null) {
-            ((TextView) findViewById(R.id.textViewDeviceID)).setText("???");
-        } else {
-            ((TextView) findViewById(R.id.textViewDeviceID)).setText(
-                    namingManager.getOwnID().getIDString()
-            );
-        }
-
-        findViewById(R.id.textViewConnectionStatus).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateConnectionStatus();
-            }
-        });
-
-        updateConnectionStatus();
-
         showRegisterQROnFirstBoot();
+
+        List<Slave> slaves = new LinkedList<>();
+        final SlaveController slaveController = getComponent(SlaveController.KEY);
+        if (slaveController != null) {
+            slaves = slaveController.getSlaves();
+        }
+        slaveList.setAdapter(new SlaveAdapter(slaves));
+
+        List<UserDevice> userDevices = new LinkedList<>();
+        final UserManagementController userController = getComponent(UserManagementController.KEY);
+        if (userController != null) {
+            userDevices = userController.getUserDevices();
+        }
+        userDeviceList.setAdapter(new UserDeviceAdapter(userDevices));
     }
 
     /**
@@ -152,37 +136,12 @@ public class MainActivity extends BoundActivity {
         }
     }
 
-    private void updateConnectionStatus() {
-        Server server = getComponent(Server.KEY);
-        String status;
-        if (server == null) {
-            status = "server not started";
-        } else {
-            final ChannelGroup channels = server.getActiveChannels();
-            status = channels.size() + " connected to " + server.getAddress() + " "
-                    + "[" + (server.isChannelOpen() ? "open" : "closed") + ", "
-                    + (server.isExecutorAlive() ? "alive" : "dead") + "]";
-            if (!channels.isEmpty()) {
-                status += ":\n";
-                for (Channel channel : channels) {
-                    final DeviceID deviceID = channel.attr(CoreConstants.NettyConstants.ATTR_PEER_ID).get();
-                    status += channel.id().asShortText() + " " + channel.remoteAddress() + " " +
-                            (deviceID != null ? deviceID.toShortString() : "???") + "\n";
-                }
-            }
-        }
-        ((TextView) findViewById(R.id.textViewConnectionStatus)).setText(status);
-    }
+
 
     @Override
     public void onContainerDisconnected() {
-        updateConnectionStatus();
     }
 
-    private void log(String text) {
-        Log.v("MASTER", text);
-        ((TextView) findViewById(R.id.textViewLog)).append(text);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -193,18 +152,245 @@ public class MainActivity extends BoundActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, MasterPreferenceActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_restart) {
+            doUnbind();
+            stopService(new Intent(this, MasterContainer.class));
+            doBind();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // FIXME use me
+    private void buildView() {
+        TextView masterID = (TextView) findViewById(R.id.mainactivity_master_masterid);
+        TextView address = (TextView) findViewById(R.id.mainactivity_master_address);
+
+        DeviceID id = getMasterID();
+        if (id != null) {
+            masterID.setText(id.toShortString());
+            address.setText(getAddress(id));
+        }
+
+        TextView connected = (TextView) findViewById(R.id.mainactivity_master_connected);
+        connected.setText(getNumberOfConnectedClients());
+
+        slaveList = (ListView) findViewById(R.id.mainactivity_master_listview_slaves);
+        slaveList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Slave item = (Slave) slaveList.getAdapter().getItem(position);
+                final Server server = getComponent(Server.KEY);
+                if (server != null) {
+                    final Channel channel = server.findChannel(item.getSlaveID());
+                    if (channel != null) {
+                        channel.close();
+                    }
+                }
+                return true;
+            }
+        });
+
+        userDeviceList = (ListView) findViewById(R.id.mainactivity_master_listview_userdevices);
+        userDeviceList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                UserDevice item = (UserDevice) userDeviceList.getAdapter().getItem(position);
+                final Server server = getComponent(Server.KEY);
+                if (server != null) {
+                    final Channel channel = server.findChannel(item.getUserDeviceID());
+                    if (channel != null) {
+                        channel.close();
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    private DeviceID getMasterID() {
+        final NamingManager component = getComponent(NamingManager.KEY);
+        if (component == null) {
+            Log.i(TAG, "Container not yet connected.");
+            return null;
+        }
+        return component.getMasterID();
+    }
+
+    private String getAddress(DeviceID id) {
+        String address = "";
+        final Server server = getComponent(Server.KEY);
+        if (server == null) {
+            Log.i(TAG, "Container not yet connected.");
+            return address;
+        }
+        Channel channelMaster = server.findChannel(id);
+        if (channelMaster != null) {
+            address = channelMaster.localAddress().toString();
+        }
+        return address;
+    }
+
+    private int getNumberOfConnectedClients() {
+        final Server server = getComponent(Server.KEY);
+        if (server == null) {
+            Log.i(TAG, "Container not yet connected.");
+            return 404;
+        }
+        ChannelGroup activeChannels = server.getActiveChannels();
+        return activeChannels.size();
+    }
+
+    private class SlaveAdapter extends BaseAdapter {
+        List<Slave> slaves;
+
+        public SlaveAdapter(List<Slave> slaves) {
+            this.slaves = slaves;
+        }
+
+        private void updateSlaveList() {
+            SlaveController handler = getComponent(SlaveController.KEY);
+
+            if (handler == null) {
+                Log.i(TAG, "Container not yet connected!");
+                return;
+            }
+
+            List<Slave> allSlaves = handler.getSlaves();
+
+            slaves = Lists.newArrayList(allSlaves);
+            Collections.sort(slaves, new Comparator<Slave>() {
+                @Override
+                public int compare(Slave lhs, Slave rhs) {
+                    if (lhs.getName() == null) {
+                        return rhs.getName() == null ? 0 : 1;
+                    }
+                    if (rhs.getName() == null) {
+                        return -1;
+                    }
+                    return lhs.getName().compareTo(rhs.getName());
+                }
+            });
+        }
+
+        @Override
+        public void notifyDataSetChanged() {
+            updateSlaveList();
+            super.notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            if (slaves != null) {
+                return slaves.size();
+            } else {
+                return 0;
+            }
+        }
+
+        @Override
+        public Slave getItem(int position) {
+            if (slaves != null) {
+                return slaves.get(position);
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            final Slave item = getItem(position);
+            if (item != null && item.getName() != null) {
+                return item.getName().hashCode();
+            } else {
+                return 0;
+            }
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // TODO Phil
+            return null;
+        }
+    }
+
+    private class UserDeviceAdapter extends BaseAdapter {
+        List<UserDevice> userDevices;
+
+        public UserDeviceAdapter(List<UserDevice> userDevices) {
+            this.userDevices = userDevices;
+        }
+
+        private void updateUserDeviceList() {
+            UserManagementController handler = getComponent(UserManagementController.KEY);
+
+            if (handler == null) {
+                Log.i(TAG, "Container not yet connected!");
+                return;
+            }
+
+            List<UserDevice> allUserDevices = handler.getUserDevices();
+
+            userDevices = Lists.newArrayList(allUserDevices);
+            Collections.sort(userDevices, new Comparator<UserDevice>() {
+                @Override
+                public int compare(UserDevice lhs, UserDevice rhs) {
+                    if (lhs.getName() == null) {
+                        return rhs.getName() == null ? 0 : 1;
+                    }
+                    if (rhs.getName() == null) {
+                        return -1;
+                    }
+                    return lhs.getName().compareTo(rhs.getName());
+                }
+            });
+        }
+
+        @Override
+        public void notifyDataSetChanged() {
+            updateUserDeviceList();
+            super.notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            if (userDevices != null) {
+                return userDevices.size();
+            } else {
+                return 0;
+            }
+        }
+
+        @Override
+        public UserDevice getItem(int position) {
+            if (userDevices != null) {
+                return userDevices.get(position);
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            final UserDevice item = getItem(position);
+            if (item != null && item.getName() != null) {
+                return item.getName().hashCode();
+            } else {
+                return 0;
+            }
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //TODO Phil
+            return null;
+        }
     }
 }
