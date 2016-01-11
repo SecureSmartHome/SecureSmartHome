9031ec8c2fe9bc6ad7e9966dfa93dc130d72ccc0
Christoph Fraedrich <fraedric@fim.uni-passau.de>
2016-01-11 15:02:58 +0100

    MasterPreferenceFragment now checks whether ports are within bounds or not

--- master/src/main/java/de/unipassau/isl/evs/ssh/master/activity/MasterPreferenceFragment.java
+++ master/src/main/java/de/unipassau/isl/evs/ssh/master/activity/MasterPreferenceFragment.java
@@ -6,12 +6,22 @@ import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
+import com.google.common.base.Strings;
+
+import net.aksingh.owmjapis.OpenWeatherMap;
+
+import java.io.IOException;
+
+import de.unipassau.isl.evs.ssh.core.CoreConstants;
 import de.unipassau.isl.evs.ssh.master.MasterConstants;
 import de.unipassau.isl.evs.ssh.master.R;
 
 /**
+ *
  * @author Niko Fink
+ * @author Christoph Frädrich
  */
 public class MasterPreferenceFragment extends PreferenceFragment implements
         SharedPreferences.OnSharedPreferenceChangeListener {
@@ -40,8 +50,6 @@ public class MasterPreferenceFragment extends PreferenceFragment implements
         }
     }
 
-
-
     @Override
     public void onResume() {
         super.onResume();
@@ -64,16 +72,42 @@ public class MasterPreferenceFragment extends PreferenceFragment implements
     private void updatePreferences(Preference p) {
         if (p instanceof EditTextPreference) {
             EditTextPreference editTextPref = (EditTextPreference) p;
-            if (editTextPref.getText() == null ||editTextPref.getText().equals("")) {
-                int resId = getResources().getIdentifier(editTextPref.getKey() + "_summary", "string",
-                        getActivity().getPackageName());
-
-                String summary = getResources().getString(resId);
-
-                p.setSummary(summary);
+            if (Strings.isNullOrEmpty(editTextPref.getText())) {
+                resetSummary(editTextPref);
             } else {
-                p.setSummary(editTextPref.getText());
+                if (editTextPref.getKey().equals(getResources().getResourceEntryName(R.string.master_port_local))
+                        || editTextPref.getKey().equals(getResources().getResourceEntryName(R.string.master_port_extern))
+                        || editTextPref.getKey().equals(getResources().getResourceEntryName(R.string.master_port_intern))) {
+                    //We can be sure this is a number due to the preference Activity only accepting numbers
+                    int port = Integer.valueOf(editTextPref.getText());
+
+                    if (port < 1024 || port > 65535) {
+                        Toast.makeText(getActivity(), R.string.invalid_port, Toast.LENGTH_SHORT).show();
+
+                        //Reset values
+                        SharedPreferences.Editor edit = getPreferenceManager().getSharedPreferences().edit();
+                        edit.putString(editTextPref.getKey(), "");
+                        edit.apply();
+                        editTextPref.setText("");
+                        resetSummary(editTextPref);
+
+                    } else {
+                        p.setSummary(editTextPref.getText());
+                    }
+
+                } else if (editTextPref.getKey().equals(getResources().getResourceEntryName((R.string.master_city_name)))) {
+                    p.setSummary(editTextPref.getText());
+                }
             }
         }
+    }
+
+    private void resetSummary(EditTextPreference p) {
+        int resId = getResources().getIdentifier(p.getKey() + "_summary", "string",
+                getActivity().getPackageName());
+
+        String summary = getResources().getString(resId);
+
+        p.setSummary(summary);
     }
 }
