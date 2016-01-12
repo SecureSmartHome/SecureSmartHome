bfb3843cc9fe5a309706d4d02a058c30d524c017
Wolfgang Popp <poppwolf@fim.uni-passau.de>
2016-01-12 23:24:56 +0100

    changed deploy script to deploy on all connected usb devices. the script is now a ZSH script

--- deploy-odroids.zsh
+++ deploy-odroids.zsh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 
 MASTER='192.168.0.100:5555'
 SLAVE='192.168.0.101:5555'
-APP=$(adb devices -l | grep -m1 usb | cut -f1 -d ' ')
+APPS=( "${(@f)$(adb devices -l | grep usb | cut -f1 -d ' ')}" )
 
 adb connect $MASTER
 adb connect $SLAVE
@@ -14,10 +14,12 @@ echo "deploying master on: $MASTER"
 adb -s $MASTER install -r ./master/build/outputs/apk/master-debug.apk
 adb -s $MASTER shell 'am start -n "de.unipassau.isl.evs.ssh.master/de.unipassau.isl.evs.ssh.master.activity.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER'
 
-echo
-echo "deploying app on: $APP"
-adb -s $APP install -r ./app/build/outputs/apk/app-debug.apk
-adb -s $APP shell 'am start -n "de.unipassau.isl.evs.ssh.app/de.unipassau.isl.evs.ssh.app.activity.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER'
+for app in "${APPS[@]}"; do
+    echo
+    echo "deploying app on: $app"
+    adb -s $app install -r ./app/build/outputs/apk/app-debug.apk
+    adb -s $app shell 'am start -n "de.unipassau.isl.evs.ssh.app/de.unipassau.isl.evs.ssh.app.activity.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER'
+done
 
 echo
 echo "deploying slave on: $SLAVE"
