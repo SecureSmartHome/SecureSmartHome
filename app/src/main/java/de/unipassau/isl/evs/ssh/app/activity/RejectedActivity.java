6997144bbc36fb998c47f89a02feb5b7adac6e46
Wolfgang Popp <poppwolf@fim.uni-passau.de>
2016-01-13 23:08:17 +0100

    new implementation of clientrejected ui

---
+++ app/src/main/java/de/unipassau/isl/evs/ssh/app/activity/RejectedActivity.java
@@ -0,0 +1,15 @@
+package de.unipassau.isl.evs.ssh.app.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.unipassau.isl.evs.ssh.app.R;
+
+public class RejectedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rejected);
+    }
+}
