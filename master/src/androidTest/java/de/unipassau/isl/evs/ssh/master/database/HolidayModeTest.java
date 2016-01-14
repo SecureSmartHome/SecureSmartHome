1e91c8103478a6aa295e429b08b51d62cd28d011
Leon Sell <sellleon@fim.uni-passau.de>
2016-01-14 16:47:04 +0100

    added test script to add holidayactions to the holiday log

---
+++ master/src/androidTest/java/de/unipassau/isl/evs/ssh/master/database/HolidayModeTest.java
@@ -0,0 +1,49 @@
+package de.unipassau.isl.evs.ssh.master.database;
+
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+
+import junit.framework.Assert;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import de.unipassau.isl.evs.ssh.core.CoreConstants;
+import de.unipassau.isl.evs.ssh.core.container.ContainerService;
+import de.unipassau.isl.evs.ssh.core.container.SimpleContainer;
+import de.unipassau.isl.evs.ssh.core.database.UnknownReferenceException;
+import de.unipassau.isl.evs.ssh.core.database.dto.Module;
+
+/**
+ * This test injects HolidayActions into the database for testing the holiday mode.
+ * For this test to work, you need to have a slave with a light module installed at the master.
+ */
+public class HolidayModeTest extends InstrumentationTestCase {
+    public void testHolidayMode() throws UnknownReferenceException {
+        Context context = getInstrumentation().getTargetContext();
+        SimpleContainer container = new SimpleContainer();
+        container.register(ContainerService.KEY_CONTEXT,
+                new ContainerService.ContextComponent(context));
+        container.register(DatabaseConnector.KEY, new DatabaseConnector());
+        container.register(HolidayController.KEY, new HolidayController());
+        container.register(SlaveController.KEY, new SlaveController());
+
+        final SlaveController slaveController = container.require(SlaveController.KEY);
+        final HolidayController holidayController = container.require(HolidayController.KEY);
+
+        List<Module> moduleList = slaveController.getModules();
+        Module lightModule = null;
+
+        for (Module module : moduleList) {
+            if (module.getModuleType().equals(CoreConstants.ModuleType.Light)) {
+                lightModule = module;
+            }
+        }
+        if (lightModule == null) {
+            Assert.fail("No light module!");
+        }
+        holidayController.addHolidayLogEntry(CoreConstants.LogActions.LIGHT_ON_ACTION, lightModule.getName(), System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(1));
+        holidayController.addHolidayLogEntry(CoreConstants.LogActions.LIGHT_OFF_ACTION, lightModule.getName(), System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(1) + TimeUnit.SECONDS.toMillis(5));
+        holidayController.addHolidayLogEntry(CoreConstants.LogActions.LIGHT_ON_ACTION, lightModule.getName(), System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(1) + TimeUnit.SECONDS.toMillis(10));
+    }
+}
